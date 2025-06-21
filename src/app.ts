f586186b55fd006a5a17780d0c8860181cacd3ee
Mohamed Shahata <125826170+Mohamed-Shahata@users.noreply.github.com>
2025-06-21 16:04:09 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -33,7 +33,8 @@ const app = express();
 // });
 
 app.use(cors({
-  origin: 'https://btix-frontend-git-main-mohameds-projects-f5551999.vercel.app'
+  origin: 'https://btix-frontend-git-main-mohameds-projects-f5551999.vercel.app',
+credentials: true
 }));
 
 app.use(express.json());
