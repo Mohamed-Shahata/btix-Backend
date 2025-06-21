0309ae844c8457838217354c495c7cc87d0530e9
Mohamed Shahata <125826170+Mohamed-Shahata@users.noreply.github.com>
2025-06-21 04:06:12 -0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -32,15 +32,7 @@ const app = express();
 //   next();
 // });
 
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://btix-frontend.vercel.app"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser())
